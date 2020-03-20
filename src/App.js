ec1173f28e2d02b89dec3a222e87fb7d7469ca81
veroena <veronica.enamorado@gmail.com>
2020-03-20 09:09:08 +0100

    Add fetch custom hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
+import {useState} from 'react';
 import './App.css';
+import Spaceships from './components/Spaceships';
+import Vehicles from './components/Vehicles';
+import Planets from './components/Planets';
 
-function App() {
+const App = props => {
+
+  const [category, setCategory] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="menu">
+        <button className="category__button category__spaceships" onClick={() => setCategory('spaceships')}>Spaceships</button>
+        <button className="category__button category__vehicles" onClick={() => setCategory('vehicles')}>Vehicles</button>
+        <button className="category__button category__planets" onClick={() => setCategory('planets')}>Planets</button>
+      </nav>
+      {category === 'spaceships' ? <Spaceships /> : null}
+      {category === 'vehicles' ? <Vehicles /> : null}
+      {category === 'planets' ? <Planets /> : null}
     </div>
-  );
+  )
 }
 
 export default App;
